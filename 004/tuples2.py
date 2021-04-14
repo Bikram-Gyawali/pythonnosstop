c09600b4c49b07e81d87c2e7ce5dbc850ad98331
Bikram Gyawali <bikramgyawali57@gmail.com>
2021-04-14 09:17:23 +0545

    tuples in python like list but unchangeable

---
+++ 004/tuples2.py
@@ -0,0 +1,16 @@
+
+# everything about tuples are same as an list but one diffrence is we cannot assign any value to the tuple every eelmens ini tuple have some meaning
+t=(1,2,3,4,5,6)
+print(t[0])
+# t[0]=9
+# print(t[0]) # we cannot do this
+
+person=('hari',30,'nepal')
+
+people=[person1,person2]
+
+name,age,nation=person
+
+print(name)
+print(age)
+print(nation)
